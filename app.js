c816b196c1748723e96e269359fa050e5bae83aa
Kevin Li <kevinli5370@live.com>
2019-02-28 18:35:16 -0500

    Added function to convert input code to ASCII binary Added function to help put data into a table with 1024 cells Added the html for the table

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 function compile(sourceCode) {
-  // do nothing for now
   return sourceCode;
+}
+
+//Formats the data in an array of 1024 strings into an 8x128 table
+//then inserts the HTML code into element
+function fillTable(table, data){
+  let text = "<tr>";
+  for(let i=0; i<1024; i++){
+    text += "<td>" + i + "</td>";
+    text += "<td>" + data[i] + "</td>";
+    if(i%8 === 7)
+      text += "</tr><tr>";  //New row
+  }
+  text += "</tr>";
+  table.innerHTML = text;
+}
+
+//Converts input to ASCII binary and populates
+//the source code part of the External Storage
+function toBinary(sourceCode, fillTable){
+  const table = document.getElementById('external-source');
+  const externalStorage = new Array(1024);
+  for(let i=0; i<sourceCode.length; i++){
+    binaryChar = sourceCode.charCodeAt(i).toString(2);
+    externalStorage[i] = binaryChar.padStart(8,"0");
+  }
+  externalStorage.fill("00000000", sourceCode.length);
+  fillTable(table, externalStorage);
 }
 
 $(document).ready(function() {
@@ -8,5 +34,6 @@ $(document).ready(function() {
     const input = $('.editor-textbox').first().val();
     const output = compile(input);
     $('.output').first().text(output);
+    toBinary(input,fillTable);
   })
 })
