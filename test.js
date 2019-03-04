9ed4589ab6cb47f595cb9a23bf43fab20df4f036
Gordon Zheng <gordon@greenhouse.io>
2019-03-04 17:23:53 -0500

    Add tests

---
+++ test.js
@@ -0,0 +1,32 @@
+const assert = function(condition, message) {
+  if (!condition) throw Error(`Assertion failed: ${message}`);
+};
+
+const TESTS = {
+  "declare variable with immediate value": function() {
+    const d = new Declaration({
+      destination: new Operand({type: "variable", value: "foo"}),
+      value: new Operand({type: "immediate", value: 5})
+    });
+    const symbolTable = {
+      "foo": -4
+    };
+    const instructions = d.toAssembly(symbolTable);
+
+    assert(instructions == ["mov DWORD PTR [rbp - 4], 5"], "unexpected output")
+
+    return true;
+  },
+}
+
+function runTests() {
+  console.log("Test results: ");
+
+  for (const [testName, fn] of Object.entries(TESTS)) {
+    const result = fn();
+    const message = result === true ? "pass" : "fail";
+    console.log(`[${message}] ${testName}`);
+  };
+}
+
+runTests();
