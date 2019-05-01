8ac4246ca66027b854ae41d5f9129683007f4b9d
Ishmaelk <Kheir1@msn.com>
2019-05-01 12:48:45 -0400

    removed a comment

--- tests/cache_test.js
+++ tests/cache_test.js
@@ -54,12 +54,5 @@ describe("cache", () => {
             // verify that replacing data is present
             expect(cache.read({ address: otherAddress })).to.equal( otherData );
         });
-      
-        /*it("should update access times", () => {
-            const memory = new Map();
-            const cache = new Cache({ nway: 1, size: 8, k: 2, memory: memory, bits: 16 });
-            cache.write({ address: '0000000000000000', data: '00000000000000000000000000000001', memwrite: true });
-            expect(cache.read({address: '0000000000000000'})).to.equal("00000000000000000000000000000001");
-        });*/
     });
   });
